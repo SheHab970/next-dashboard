0bed80c1f018c1ee0cbe1d8ee7caea3bf767d4dc
SheHab970 <shehab202205097@gmail.com>
2025-11-30 14:46:11 +0200

    Install and test shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-5xl font-bold">Welcome to Next.js!</h1>
-        <p className="mt-4 text-lg">Get started by editing <code>src/app/page.tsx</code></p>
-        <div className="mt-8">
-          <Image
-            src="/next-logo.png"
-            alt="Next.js Logo"
-            width={200}
-            height={200}
-          />
-        </div>
+        <Button>Click Me</Button>
       </main>  
     </>
   );
