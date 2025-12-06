46267a419f82721e330bbea6b203bc13983f5e32
SheHab970 <shehab202205097@gmail.com>
2025-12-06 20:20:14 +0200

    Add TodoList component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import AppAreaChart from "@/components/AppAreaChart";
 import AppBarChart from "@/components/AppBarChart";
 import AppPieChart from "@/components/AppPieChart";
 import CardList from "@/components/CardList";
+import TodoList from "@/components/TodoList";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { CircleArrowOutUpRight } from "lucide-react";
@@ -22,13 +23,15 @@ export default function Home() {
           <div className=" bg-primary-foreground p-4 rounded-lg">
             <AppPieChart />
           </div>
+          <div className=" bg-primary-foreground p-4 rounded-lg">
+            <TodoList />
+          </div>
           <div className=" bg-primary-foreground p-4 rounded-lg lg:col-span-2">
             <AppAreaChart />
           </div>
           <div className=" bg-primary-foreground p-4 rounded-lg">
             <CardList title="Popular Content"/>
           </div>
-          <div className=" bg-primary-foreground p-4 rounded-lg">Test</div>
       </main>  
     </>
   );
