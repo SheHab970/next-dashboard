d56cd0cb85ec4ac38a4bb10ce104c334c3d62312
SheHab970 <shehab202205097@gmail.com>
2025-12-06 18:33:18 +0200

    Add cardLlist component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import AppAreaChart from "@/components/AppAreaChart";
 import AppBarChart from "@/components/AppBarChart";
 import AppPieChart from "@/components/AppPieChart";
+import CardList from "@/components/CardList";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { CircleArrowOutUpRight } from "lucide-react";
@@ -15,14 +16,18 @@ export default function Home() {
           <div className=" bg-primary-foreground p-4 rounded-lg lg:col-span-2">
             <AppBarChart />
           </div>
-          <div className=" bg-primary-foreground p-4 rounded-lg">Test</div>
+          <div className=" bg-primary-foreground p-4 rounded-lg">
+            <CardList title="Latest Transactions"/>
+          </div>
           <div className=" bg-primary-foreground p-4 rounded-lg">
             <AppPieChart />
           </div>
           <div className=" bg-primary-foreground p-4 rounded-lg lg:col-span-2">
             <AppAreaChart />
           </div>
-          <div className=" bg-primary-foreground p-4 rounded-lg">Test</div>
+          <div className=" bg-primary-foreground p-4 rounded-lg">
+            <CardList title="Popular Content"/>
+          </div>
           <div className=" bg-primary-foreground p-4 rounded-lg">Test</div>
       </main>  
     </>
