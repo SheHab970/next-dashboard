e7fb6ebee6bd391b03a8a05c696e209406f9d2c1
SheHab970 <shehab202205097@gmail.com>
2025-12-06 19:05:17 +0200

    Eddite cards in CardList component

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Card, CardContent, CardFooter, CardTitle } from "./ui/card";
+import { Badge } from "./ui/badge";
 
 const popularContent = [
   {
@@ -95,16 +96,17 @@ const CardList = ({title} : {title : string}) => {
             <h1 className="text-lg font-medium mb-6">{title}</h1>
             <div className="flex flex-col gap-2">
                 {list.map(item => (
-                    <Card key={item.id}>
+                    <Card key={item.id} className="flex-row justify-between items-center gap-4 p-4">
                         <div className="w-12 h-12 rounded-sm relative overflow-hidden">
                             <Image src={item.image} alt={item.title} fill className=" object-cover" />
                         </div>
-                        <CardContent>
-                            <CardTitle>
+                        <CardContent className="flex-1 p-0">
+                            <CardTitle className="text-sm font-medium mb-2">
                                 {item.title}
                             </CardTitle>
+                            <Badge variant="secondary">{item.badge}</Badge>
                         </CardContent>
-                        <CardFooter>
+                        <CardFooter className="p-0">
                             {item.count / 1000}K
                         </CardFooter>
                     </Card>
