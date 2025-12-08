d4ca3b21567b7d51a2e9a5b26940f9a3134c9447
SheHab970 <shehab202205097@gmail.com>
2025-12-08 22:38:13 +0200

    Make nav position sticky

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
       const { toggleSidebar } = useSidebar()
 
     return (
-        <nav className="flex items-center justify-between p-4">
+        <nav className="flex items-center justify-between p-4 sticky top-0 bg-background z-10">
             {/* LEFT  */}
             <SidebarTrigger />
 
