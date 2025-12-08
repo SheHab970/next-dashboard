b24de89f838e48d6ae904e5cb6c8b609ecbd9802
SheHab970 <shehab202205097@gmail.com>
2025-12-08 19:49:31 +0200

    Add button as sheet component and create EditeUser component

---
+++ src/components/EditeUser.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { SheetContent, SheetDescription, SheetHeader, SheetTitle } from "./ui/sheet";
+
+const EditeUser = () => {
+    return(
+        <>
+            <SheetContent>
+                <SheetHeader>
+                <SheetTitle>Are you absolutely sure?</SheetTitle>
+                <SheetDescription>
+                    This action cannot be undone. This will permanently delete your account
+                    and remove your data from our servers.
+                </SheetDescription>
+                </SheetHeader>
+            </SheetContent>
+        </>
+    );
+}
+
+export default EditeUser;
